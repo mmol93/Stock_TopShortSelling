042ccd59e17e3f1d252dd94ea7b9f89520638e82
Ljw <ljws93@naver.com>
2021-05-12 15:47:15 +0900

    set Kospi & Kosdaq shortSelling top 50

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+from pykrx import stock
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# 참조 https://github.com/sharebook-kr/pykrx
+
+# DataFrame의 티커에서 종목이름으로 변경하기
+def check(KOSPIShort, KOSDAQShort):
+    # DataFrame의 인덱스 자체가 종목 코드임
+    KOSPIStockNumber = KOSPIShort.index
+    KOSDAQStockNumber = KOSDAQShort.index
+
+    # 티커를 종목명으로 출력
+    # 코스피 티커
+    for ticker in KOSPIStockNumber:
+        stockName = stock.get_market_ticker_name(ticker)
+        print(stockName)
+
+    # 코스닥 티커
+    for ticker in KOSDAQStockNumber:
+        stockName = stock.get_market_ticker_name(ticker)
+        print(stockName)
+
+# 공매도 거래 비중 top50
+KOSPIShortVolume = stock.get_shorting_volume_top50("20201104", "KOSPI")
+KOSDAQShortVolume = stock.get_shorting_volume_top50("20201104", "KOSDAQ")
+check(KOSPIShortVolume, KOSDAQShortVolume)
+
+# 공매도 잔고 top50
+KOSPIShortBalance = stock.get_shorting_balance_top50("20201104", "KOSPI")
+KOSDAQShortBalance = stock.get_shorting_balance_top50("20201104", "KOSDAQ")
+check(KOSPIShortBalance, KOSDAQShortBalance)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
