837c4e39a0bfbcec383ffec2c139bcfce1c66ece
Ljw <ljws93@naver.com>
2021-05-12 16:31:21 +0900

    add print function for let know finish

--- main.py
+++ main.py
@@ -25,15 +25,16 @@ def check(KOSPIShort, KOSDAQShort):
 
 # 오늘 날짜 가져오기
 today = datetime.today().strftime("%Y%m%d")  # YYYYmmddHHMMSS 형태의 시간 출력
-print(today)
 
 # 공매도 "거래 비중" top50을 가져와서 종목코드를 리스트 형태로 담는다
-KOSPIShortVolume = stock.get_shorting_volume_top50("20201104", "KOSPI")
-KOSDAQShortVolume = stock.get_shorting_volume_top50("20201104", "KOSDAQ")
+KOSPIShortVolume = stock.get_shorting_volume_top50(today, "KOSPI")
+KOSDAQShortVolume = stock.get_shorting_volume_top50(today, "KOSDAQ")
 # 종목 코드를 바탕으로 종목명을 알아내고 엑셀에서 해당 종목이 있을 경우 삭제
 check(KOSPIShortVolume, KOSDAQShortVolume)
+print("거래 비중 기준 삭제 끝")
 
 # 공매도 "잔고" top50을 가져와서 종목코드를 리스트 형태로 담는다
-KOSPIShortBalance = stock.get_shorting_balance_top50("20201104", "KOSPI")
-KOSDAQShortBalance = stock.get_shorting_balance_top50("20201104", "KOSDAQ")
+KOSPIShortBalance = stock.get_shorting_balance_top50(today, "KOSPI")
+KOSDAQShortBalance = stock.get_shorting_balance_top50(today, "KOSDAQ")
 check(KOSPIShortBalance, KOSDAQShortBalance)
+print("잔고 기준 삭제 끝")
